aaf8bb20d659e06f01de684341f43e7e9111f60c
Gonzalo Peralta <gonnzaperalta94@gmail.com>
2024-12-31 02:11:29 -0300

    clases y herencias creadas Categoria - Paquete - Cliente, Cotizacion, Flete, Transportista

--- src/traslados/admin.py
+++ src/traslados/admin.py
@@ -2,4 +2,28 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Categoria)
+admin.site.register(models.Categoria) 
+
+@admin.register(models.Paquete)
+class PaqueteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion', 'distancia', 'categoria')
+
+    
+@admin.register(models.Cotizacion)
+class CotizacionAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'descripcion', 'precio_por_km', 'paquete', 'total', 'fecha_de_entrega')
+
+
+@admin.register(models.Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'direccion', 'telefono', 'email', 'paquete')
+
+
+@admin.register(models.Flete)
+class FleteAdmin(admin.ModelAdmin):
+    list_display = ('cliente',)
+
+
+@admin.register(models.Transportista)
+class TransportistaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'licencia', 'telefono', 'email', 'flete')
--- src/traslados/models.py
+++ src/traslados/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 class Categoria(models.Model):
@@ -7,3 +8,82 @@ class Categoria(models.Model):
 
     def __str__(self):
         return f'{self.nombre} - {self.descripcion}'
+    
+    class Meta:
+        verbose_name = 'Categoria de Paquetes'
+        verbose_name_plural = 'Categorias de Paquetes'
+
+  
+
+class Paquete(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.TextField()
+    distancia = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.nombre} - {self.descripcion} - {self.categoria} '
+    
+    class Meta:
+        verbose_name = 'Paquete'
+        verbose_name_plural = 'Paquetes'
+        
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    apellido = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=50)
+    email = models.EmailField()
+    paquete = models.ForeignKey(Paquete, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.nombre} - {self.apellido} - {self.direccion} - {self.telefono} - {self.email}'
+    
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        
+class Cotizacion(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
+    descripcion = models.TextField()
+    precio_por_km = models.IntegerField(default=1400)
+    paquete = models.ForeignKey(Paquete, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    fecha_de_entrega = models.DateField(null=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        self.total = self.paquete.distancia * self.precio_por_km
+        super(Cotizacion, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.cliente} - {self.descripcion} - {self.total}'
+    
+    class Meta:
+        verbose_name = 'Cotizacion'
+        verbose_name_plural = 'Cotizaciones'   
+        
+
+class Flete(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.cliente}'
+    
+    class Meta:
+        verbose_name = 'Flete'
+        verbose_name_plural = 'Fletes'
+
+class Transportista(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    apellido = models.CharField(max_length=50)
+    licencia = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=50)
+    email = models.EmailField()
+    flete = models.ForeignKey(Flete, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.nombre} - {self.apellido} - {self.licencia} - {self.telefono} - {self.email}'
+    
+    class Meta:
+        verbose_name = 'Transportista'
+        verbose_name_plural = 'Transportistas'
